4b19cead529f82e5d5c0e99db2faab760b342092
Danilo-GB <rubendanilo.garcia@gmail.com>
2022-05-23 10:50:52 -0500

    minor changes

--- app/Http/Controllers/UserDatabaseController.php
+++ app/Http/Controllers/UserDatabaseController.php
@@ -32,11 +32,11 @@ class UserDatabaseController extends Controller
     public function addDatabase(Request $request)
     {
         $data = [
-            'DB_USER_HOST' => $request->dbHost,
-            'DB_USER_PORT' => $request->dbPort,
-            'DB_USER_DATABASE' => $request->dbName,
-            'DB_USER_USERNAME' => $request->dbUsername,
-            'DB_USER_PASSWORD' => $request->dbPassword
+            'host' => $request->dbHost,
+            'port' => $request->dbPort,
+            'database' => $request->dbName,
+            'username' => $request->dbUsername,
+            'password' => $request->dbPassword
         ];
 
         $DBregister = json_decode(Storage::get('databaseRegister.json'), true);
